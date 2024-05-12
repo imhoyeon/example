c872d30aa5286a8bd45df18349484dbf909b021c
imhoyeon <hyshin5790@gmail.com>
2024-05-13 00:33:50 +0900

    랜덤티피

---
+++ src/main/java/me/shy/learnplugin/Learnplugin.java
@@ -0,0 +1,34 @@
+package me.shy.learnplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import java.util.Random;
+
+import static java.lang.Math.random;
+
+public final class Learnplugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getLogger().warning("플러그인 활성화");
+
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getLogger().warning("플러그인 비활성화");
+    }
+
+    public boolean randomTeleport(){
+
+        Random random = new Random();
+        int x = random.nextInt(10000) - 10000;
+        int z = random.nextInt(10000) - 10000;
+
+    }
+}
